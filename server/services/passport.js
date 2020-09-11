c15f3e55f82e4e8d1a0917fe1c6097da0778085f
Jonatahn Bajada <jbajada@ryerson.ca>
2020-09-11 12:08:01 -0400

    backend

--- server/services/passport.js
+++ server/services/passport.js
@@ -23,11 +23,11 @@ passport.use(
 			callbackURL: '/auth/google/callback'
 		},
 		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleID: profile.id }).then((existingUser) => {
+			User.findOne({ googleId: profile.id }).then((existingUser) => {
 				if (existingUser) {
 					done(null, existingUser);
 				} else {
-					new User({ googleID: profile.id }).save().then((user) => done(null, user));
+					new User({ googleId: profile.id }).save().then((user) => done(null, user));
 				}
 			});
 		}
