8238841aeaf75ef822d5661507a807759d027be0
Jonatahn Bajada <jbajada@ryerson.ca>
2020-09-14 15:39:13 -0400

    client

---
+++ server/config/dev.js
@@ -0,0 +1,7 @@
+//dev.js - don't commit
+module.exports = {
+	googleClientID: '750061771212-5115d9v2mncmfn410p8302dje455lcvd.apps.googleusercontent.com',
+	googleClientSecret: 'IkwBeToIZcx_yXa97XwtyhCE',
+	mongoURI: 'mongodb+srv://admin:ZwEU2J3p1mCwTWIg@emaily.typsf.mongodb.net/users?retryWrites=true&w=majority',
+	cookieKey: 'sdfadfasfafdaf'
+};
