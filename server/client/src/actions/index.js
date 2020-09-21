7c219eb1c93e769326226f050376e5e7cc127c0d
Jonatahn Bajada <jbajada@ryerson.ca>
2020-09-21 19:49:05 -0400

    stripe

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 //whenever action creater calls return function, redux-thunk will auto call dispatch
-export const fetchUser = () =>
-	function(dispatch) {
-		axios.get('/api/current_user').then((res) => dispatch({ type: FETCH_USER, payload: res }));
-	};
+export const fetchUser = () => async (dispatch) => {
+	const res = await axios.get('/api/current_user');
+	dispatch({ type: FETCH_USER, payload: res.data });
+};
--- server/config/dev.js
+++ server/config/dev.js
@@ -3,5 +3,9 @@ module.exports = {
 	googleClientID: '750061771212-5115d9v2mncmfn410p8302dje455lcvd.apps.googleusercontent.com',
 	googleClientSecret: 'IkwBeToIZcx_yXa97XwtyhCE',
 	mongoURI: 'mongodb+srv://admin:ZwEU2J3p1mCwTWIg@emaily.typsf.mongodb.net/users?retryWrites=true&w=majority',
-	cookieKey: 'sdfadfasfafdaf'
+	cookieKey: 'sdfadfasfafdaf',
+	stripePublishableKey:
+		'pk_test_51HTyNWCfSSwCZOBAEbilBBq6BHqSCUReL2Afm4gunPM4EBfKZq0wnYNS3UFNzoWl6gNSUrPjML4bOuH7tIqibScG00cNQj8jEJ',
+	stripeSecretKey:
+		'sk_test_51HTyNWCfSSwCZOBAtsWZJvznFvhaJC6KqpIlUUoilLWVNYiRUkowtBlVKG1mUlVQnK0W8psycZonHCr4ZYJfcPNi007ivb7l0r'
 };
